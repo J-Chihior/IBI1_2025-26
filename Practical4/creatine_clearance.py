84efb86d5307254ee44c5a9024bf0139436e1ade
Jin Chenxi <chenxi.25@intl.zju.edu.cn>
2026-03-11 13:44:28 +0800

    Practical4

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -4,19 +4,24 @@
 # 4 if femal: multiply by 0.85
 # 5 print esult
 # 6 if invalid, print the exact error
-age = 25
-weight = 120
-gender = "male"
-Cr = 80
+age = int(input("please enter your age"))
+weight = float(input("please enter your weight, in kg"))
+gender = input("please enter your gender")
+Cr = float (input("please enter your creation concentration, in µmol/l"))
+error = False
 if age >= 100:
     print ("Error:age should be less than 100")
-elif weight <= 20 or weight>= 80 :
+    error = True
+if weight <= 20 or weight>= 80 :
     print ("Error: weight should be between 20 and 80")
-elif Cr <= 0 or Cr >= 100 :
-    print(" Error: creation concentration should be between 0 and 100 ")
-elif gender != "female" and gender != "male":
+    error = True
+if Cr <= 0 or Cr >= 100 :
+    print("Error: creation concentration should be between 0 and 100 ")
+    error = True
+if gender != "female" and gender != "male":
     print("Error: gender should be either male or female")
-else:
+    error = True
+if error == False:
     CrCl = ((140-age)* weight)/ (72* Cr) 
     if gender == "female" :
         CrCl = CrCl * 0.85
